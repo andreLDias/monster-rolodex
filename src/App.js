3cd5f3a35da5e74c4001a835edb0bd5a90624290
Andre Dias <andre.dias@provi.com.br>
2020-10-23 20:36:10 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component.jsx';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
+  }
+
+  handleChange(e) {
+    this.setState({ searchField: e.target.value })
+  }
+
+  // Same as:
+  // handleChange = e => {
+  //   this.setState({ searchField: e.target.value })
+  // }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => {
+      return monster.name.toLowerCase().includes(searchField.toLowerCase())
+    });
+    return (
+      <div className='App'>
+        <SearchBox 
+          placeholder= 'search-monster'
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters}/>
+      </div>
+    );
+  }
+}
+
+export default App;
