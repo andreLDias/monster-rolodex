1d9a6729791aec330be1b7ea0ef8545d8b0c52fc
Andre Dias <andre.dias@provi.com.br>
2020-10-23 21:04:44 -0300

    Updates

---
+++ precache-manifest.2de952ea12511b63172d51200372fa9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f35fcd1d56fc83eaf5ad7828d30bfc5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b1f340c1770bf3cbfac0",
+    "url": "/monsters-rolodex/static/css/main.f19bcc8a.chunk.css"
+  },
+  {
+    "revision": "a5ca0975cc8b8a97cbcf",
+    "url": "/monsters-rolodex/static/js/2.78041ca6.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/monsters-rolodex/static/js/2.78041ca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1f340c1770bf3cbfac0",
+    "url": "/monsters-rolodex/static/js/main.aea19c90.chunk.js"
+  },
+  {
+    "revision": "dfba82531f669c6858b0",
+    "url": "/monsters-rolodex/static/js/runtime-main.589ddcef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
